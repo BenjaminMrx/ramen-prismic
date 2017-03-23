8cd1d9b4fe4f2253401a7b17dbecfe3b3cdeffd1
Charles Tatibouet <charles.tatibouet@inconito.fr>
2017-03-23 16:50:26 +0100

    prismic files

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = function() {
 
   // all environments
   app.set('port', process.env.PORT || 3000);
-  app.set('views', path.join(__dirname, 'views'));
+  app.set('views', path.join(__dirname, 'src/templates'));
   app.set('view engine', 'pug');
   app.use(favicon('public/images/punch.png'));
   app.use(logger('dev'));
